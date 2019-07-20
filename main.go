91da632fa428e07363f9ce1f54ae4c707c6a0ec6
John Godsey <johngodsey@gmail.com>
2019-07-19 20:48:10 -0400

    Allow multiple clients

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 
 var port = flag.String("port", ":9999", "Port rocky clients connect to")
 var clientPort = flag.String("client-port", ":5000", "Port real clients connect to")
+var connections map[string]net.Conn
 
 func main() {
+	connections = make(map[string]net.Conn)
 	flag.Parse()
 	ln, err := net.Listen("tcp", *port)
 	if err != nil {
@@ -23,12 +25,33 @@ func main() {
 		return
 	}
 
+	proxyLn, err := net.Listen("tcp", ":9998")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	go handleProxy(proxyLn)
+
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			// handle error
 		}
 		go handleIncomingClients(conn, ln2)
+	}
+}
+
+func handleProxy(ln net.Listener) {
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Proxy thread failed")
+			return
+		}
+		buf := make([]byte, 10)
+		conn.Read(buf)
+		id := string(buf)
+		connections[id] = conn
 	}
 }
 
@@ -59,13 +82,26 @@ func handleFromTarget(conn net.Conn, targetConn net.Conn) {
 
 func handleIncomingClients(targetConn net.Conn, ln net.Listener) {
 	fmt.Println("Handling new clients.")
+	defer fmt.Println("Incoming Thread Died")
 	for {
 		clientConn, err := ln.Accept()
 		if err != nil {
-			// handle error
+			return
 		}
 		fmt.Println("New connection")
-		go handleToTarget(clientConn, targetConn)
-		go handleFromTarget(clientConn, targetConn)
+
+		targetConn.Write([]byte("New\n"))
+		targetConn.Write([]byte("1\n"))
+		fmt.Println("SEnd id")
+		buf := make([]byte, 10)
+		targetConn.Read(buf)
+		id := string(buf)
+		fmt.Println("ID", id)
+		for connections[id] == nil {
+			//fmt.Println(connections)
+		}
+
+		go handleToTarget(clientConn, connections[id])
+		go handleFromTarget(clientConn, connections[id])
 	}
 }
