36fe970ef1dce62c5b5d6c3c70ca86f436deefb6
John Godsey <johngodsey@gmail.com>
2022-03-09 22:26:53 -0500

    Add country and ip blocking

--- app/app.go
+++ app/app.go
@@ -8,12 +8,15 @@ import (
 	"flag"
 	"io/ioutil"
 	"net"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/jaeg/rocky-server/proxy"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/jpillora/ipfilter"
 )
 
 const AppName = "rocky-server"
@@ -29,12 +32,18 @@ var tunnelPort = flag.String("tunnel-port", ":9998", "Port that is used for indi
 var serverPort = flag.String("server-port", ":9999", "Port rocky clients connect to for management")
 var proxyPort = flag.String("proxy-port", ":8099", "Port real clients connect to IE the exposed port")
 
+var allowedIPs = flag.String("allowed-ips", "", "List of ips to allow.  Blank to allow all")
+var blockedIPs = flag.String("blocked-ips", "", "List of ips to block.")
+
+var blockedCountries = flag.String("blocked-countries", "", "List of countries to block.")
+
 type App struct {
 	tunnels        map[string]net.Conn
 	tunnelLock     *sync.Mutex
 	tunnelListener net.Listener
 	serverListener net.Listener
 	proxyListener  net.Listener
+	ipFilter       *ipfilter.IPFilter
 }
 
 func (a *App) Init() {
@@ -51,6 +60,34 @@ func (a *App) Init() {
 
 	var err error
 
+	//Setup filter
+	a.ipFilter = ipfilter.New(ipfilter.Options{BlockByDefault: *allowedIPs != ""})
+
+	if *allowedIPs != "" {
+		ips := strings.Split(*allowedIPs, ",")
+		for _, v := range ips {
+			log.WithField("IP", v).Info("Allowing ip")
+			a.ipFilter.AllowIP(v)
+		}
+	}
+
+	if *blockedIPs != "" {
+		ips := strings.Split(*blockedIPs, ",")
+		for _, v := range ips {
+			log.WithField("IP", v).Info("Blocking ip")
+			a.ipFilter.BlockIP(v)
+		}
+	}
+
+	if *blockedCountries != "" {
+		countries := strings.Split(*blockedCountries, ",")
+		for _, v := range countries {
+			log.WithField("Country", v).Info("Blocking country")
+
+			a.ipFilter.BlockCountry(v)
+		}
+	}
+
 	if *proxyCertFile != "" {
 		log.Info("Start proxy listener with cert %s", *proxyCertFile)
 		cert, err := tls.LoadX509KeyPair(*proxyCertFile, *proxyKeyFile)
@@ -59,8 +96,10 @@ func (a *App) Init() {
 			return
 		}
 		config := tls.Config{Certificates: []tls.Certificate{cert}}
+
 		config.Rand = rand.Reader
 		a.proxyListener, err = tls.Listen("tcp", *proxyPort, &config)
+
 		if err != nil {
 			log.WithError(err).Error("tls proxy listener failed")
 			return
@@ -135,8 +174,21 @@ func (a *App) Run(ctx context.Context) {
 				if err != nil {
 					log.WithError(err).Error("Error accepting incoming message on server port")
 				} else {
-					//New client with traffic that needs forwarded to it.
-					go a.handleClient(conn)
+					ip := strings.Split(conn.RemoteAddr().String(), ":")
+					if len(ip) > 0 {
+						log.WithField("IP", ip[0]).Debug("Incoming client connection")
+						if a.ipFilter.Allowed(ip[0]) {
+							//New client with traffic that needs forwarded to it.
+							go a.handleClient(conn)
+						} else {
+							log.WithField("IP", ip[0]).Error("IP blocked on client port")
+							conn.Close()
+						}
+
+					} else {
+						log.WithField("IP", ip[0]).Error("Invalid ip on client port")
+						conn.Close()
+					}
 				}
 			}
 		}
@@ -158,6 +210,20 @@ func (a *App) handleTunnelListener(tunnelListener net.Listener) {
 			return
 		}
 
+		//Verify conn's origin
+		ip := strings.Split(conn.RemoteAddr().String(), ":")
+		if len(ip) > 0 {
+			if a.ipFilter.Blocked(ip[0]) {
+				log.WithField("IP", ip[0]).Error("Blocked ip coming into tunnel port")
+				conn.Close()
+				continue
+			}
+		} else {
+			log.WithField("IP", ip[0]).Error("Invalid ip coming into tunnel port")
+			conn.Close()
+			continue
+		}
+
 		// First message in the tunnel is an ID.
 		buf := make([]byte, 36)
 		_, err = conn.Read(buf)
@@ -177,58 +243,59 @@ func (a *App) handleTunnelListener(tunnelListener net.Listener) {
 
 //Handles listening for incoming proxy clients that want to expose their targets.
 func (a *App) handleClient(clientManagementConn net.Conn) {
-	log.Debug("Handling new client")
-	defer log.Info("handleClient thread died")
+	myLogger := log.WithField("IP", clientManagementConn.RemoteAddr().String())
+	myLogger.Debug("Handling new client")
+	defer myLogger.Info("handleClient thread died")
 
 	for {
 		//Accept incoming traffic to proxy.
 		incomingRequestConn, err := a.proxyListener.Accept()
 
 		if err != nil {
-			log.WithError(err).Error("Error accepting proxy client")
+			myLogger.WithError(err).Error("Error accepting proxy client")
 			continue
 		}
 
-		log.Info("New connection to proxy")
+		myLogger.Info("New connection to proxy")
 		//Generate a new id and send it to the proxy client to signal it to create a new tunnel connection.
 		generatedId := uuid.New().String()
 		_, err = clientManagementConn.Write([]byte("New\n"))
 
 		if err != nil {
-			log.WithField("Id", generatedId).WithError(err).Error("Failed writing to management port")
+			myLogger.WithField("Id", generatedId).WithError(err).Error("Failed writing to management port")
 			incomingRequestConn.Close()
 			return
 		}
 
 		_, err = clientManagementConn.Write([]byte(generatedId + "\n"))
 		if err != nil {
-			log.WithField("Id", generatedId).WithError(err).Error("Failed writing to management port")
+			myLogger.WithField("Id", generatedId).WithError(err).Error("Failed writing to management port")
 			incomingRequestConn.Close()
 			return
 		}
 
-		log.WithField("Id", generatedId).Debug("Sent generated id to proxy client, waiting for response")
+		myLogger.WithField("Id", generatedId).Debug("Sent generated id to proxy client, waiting for response")
 
 		// Wait for the response from the proxy client and confirm IDs.
 		buf := make([]byte, len(generatedId))
 		_, err = clientManagementConn.Read(buf)
 
 		if err != nil {
-			log.WithField("Id", generatedId).WithError(err).Error("Failed reading from management port")
+			myLogger.WithField("Id", generatedId).WithError(err).Error("Failed reading from management port")
 			incomingRequestConn.Close()
 			return
 		}
 
 		id := string(buf)
-		log.WithField("Id", id).Debug("ID returned from the proxy client, checking for match.")
+		myLogger.WithField("Id", id).Debug("ID returned from the proxy client, checking for match.")
 		if id != generatedId {
-			log.WithFields(log.Fields{"ID": id, "Gen ID": generatedId}).Error("Proxy failed, mismatched IDs")
+			myLogger.WithFields(log.Fields{"ID": id, "Gen ID": generatedId}).Error("Proxy failed, mismatched IDs")
 			incomingRequestConn.Close()
 			continue
 		}
 
 		//Wait for the proxy client to open a tunnel between it and this server.
-		log.WithField("Id", id).Debug("Waiting for connection from proxy client")
+		myLogger.WithField("Id", id).Debug("Waiting for connection from proxy client")
 		t := time.Now()
 		for time.Since(t) < time.Second {
 			a.tunnelLock.Lock()
@@ -243,14 +310,14 @@ func (a *App) handleClient(clientManagementConn net.Conn) {
 		//Check to see if we got a connection within the timeout.
 		a.tunnelLock.Lock()
 		if a.tunnels[id] == nil {
-			log.WithField("Id", id).Error("Timed out waiting for connection from client")
+			myLogger.WithField("Id", id).Error("Timed out waiting for connection from client")
 			a.tunnelLock.Unlock()
 			continue
 		}
 		a.tunnelLock.Unlock()
 
 		//Create the thread to proxy the data through the tunnel.
-		log.WithField("Id", id).Debug("Connection made with client, creating proxy thread.")
+		myLogger.WithField("Id", id).Debug("Connection made with client, creating proxy thread.")
 		proxy.NewProxyThread(id, incomingRequestConn, a.tunnels[id])
 	}
 }
--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.17
 
 require (
 	github.com/google/uuid v1.3.0
+	github.com/jpillora/ipfilter v1.2.3
 	github.com/sirupsen/logrus v1.8.1
 )
 
 require (
 	github.com/google/logger v1.1.1 // indirect
+	github.com/phuslu/iploc v1.0.20210908 // indirect
+	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce // indirect
 	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
 )
